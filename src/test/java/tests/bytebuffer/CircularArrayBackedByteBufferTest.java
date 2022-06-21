0982b1e0218e6db53abe219cb22f25ac492931b9
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2022-06-21 16:21:41 -0700

    trying different approach to resizing array which should keep all elements, even those in non-readable zones

--- src/test/java/tests/bytebuffer/CircularArrayBackedByteBufferTest.java
+++ src/test/java/tests/bytebuffer/CircularArrayBackedByteBufferTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CircularArrayBackedByteBufferTest {
 
